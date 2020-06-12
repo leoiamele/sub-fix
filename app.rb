c9d1d2fa758e365e7001f3891c91108b5b38b343
Leandro Iamele <leandroiamele@gmail.com>
2020-06-12 19:30:47 -0300

    Replace all letters

--- app.rb
+++ app.rb
@@ -3,8 +3,9 @@ temp = ""
 
 File.read("users.txt").split().each do |w|
   w.split(%r{\s*}).each do |l|
-    temp << l.gsub("ñ", "n")
-    
+    temp << l.gsub("ñ", "n").gsub("Ñ", "N").gsub("á", "a").gsub("Á", "A")
+    .gsub("é", "e").gsub("É", "E").gsub("í", "i").gsub("Í", "I")
+    .gsub("ó", "o").gsub("Ó", "o").gsub("ú", "u").gsub("Ú", "u")
  end
  temp +=" "
 end
