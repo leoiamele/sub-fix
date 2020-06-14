11fad02b1ed63c234133fed9b287e3f7d5a0416a
Leandro Iamele <leandroiamele@gmail.com>
2020-06-13 23:59:26 -0300

    New ruby version - Needs Refactoring

--- app.rb
+++ app.rb
@@ -1,30 +1,8 @@
-
-temp = "" 
-
-File.read("users.txt").split().each do |w|
-  w.split(%r{\s*}).each do |l|
-    temp << l.gsub("ñ", "n").gsub("Ñ", "N").gsub("á", "a").gsub("Á", "A")
-    .gsub("é", "e").gsub("É", "E").gsub("í", "i").gsub("Í", "I")
-    .gsub("ó", "o").gsub("Ó", "o").gsub("ú", "u").gsub("Ú", "u")
- end
- temp +=" "
+File.foreach("users.srt") do |line| 
+  File.write("log2.txt", line.gsub("ñ", "n").gsub("Ñ", "N").gsub("á", "a").gsub("Á", "A")
+      .gsub("é", "e").gsub("É", "E").gsub("í", "i").gsub("Í", "I")
+      .gsub("ó", "o").gsub("Ó", "o").gsub("ú", "u").gsub("Ú", "u"), mode: "a")
 end
-new_file = []  
-new_file << temp
-p new_file
-
-
-# .first().split(%r{\s*}).each do |l|
-#    temp << l.gsub("ñ", "n")
-# end
-# new_file = []  
-# new_file << temp +" "
-# p new_file
-
-# .split(%r{\s*}).each do |l|
-#   #l.gsub("ñ", "n")
-#   p l
-# end
 
 
 
