bcf329692a0750d0e9edb628290281ef3cd8137d
Leandro Iamele <leandroiamele@gmail.com>
2020-06-14 00:03:57 -0300

    Remove JS and HTML, now using Ruby with console

--- app.js
+++
@@ -1,8 +0,0 @@
-import System.IO;
-
-const button = document.getElementById('button');
-const input = document.getElementById('sub-file').value;
-
-button.addEventListener('click', () => {
-  console.log(input.value);
-})
